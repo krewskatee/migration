dbc42844f26eefe66533efe4ca4661cff2dad862
krewskatee <lchira99@gmail.com>
2017-09-07 20:20:35 -0500

    first commit

---
+++ migration-app/db/migrate/20170908002736_make_changes_to_products.rb
@@ -0,0 +1,8 @@
+class MakeChangesToProducts < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :products, :title, :name
+    change_column :products, :description, :text
+    change_column :products, :price, "numeric USING CAST(price AS numeric)", precision: 7, scale: 2
+    remove_column :products, :isbn, :string
+  end
+end
---
+++ migration-app/db/seeds.rb
@@ -0,0 +1,3 @@
+Product.create!([
+  {name: "trex shirt", description: nil, price: "5.99", stock: nil}
+])
